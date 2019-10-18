3f900be94d010bd546ae49288ef01533e1c82810
Jenn <hjenniffer@hotmail.com>
2019-10-18 09:43:04 -0700

    adding Show Function

--- js/script.js
+++ js/script.js
@@ -9,26 +9,8 @@ FSJS project 2 - List Filter and Pagination
 //global variables
 
 const studentListItem = document.getElementsByClassName("student-item cf");
-console.log(studentListItem.length);
+const itemsPerPage= 10;
 
-const itemPerPage= 10;
-
-
-console.log(itemPerPage);
-
-
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
 
 
 
@@ -46,6 +28,56 @@ console.log(itemPerPage);
        that will be passed into the parens later when you call or 
        "invoke" the function 
 ***/
+
+// create a function 
+
+const showPage = (list,page) => {
+
+   // Loop over items in the list paramter // create two local constant to store start index and end Index
+
+   let startIndex = (page * itemsPerPage) - itemsPerPage;
+   let endIndex = page * itemsPerPage;
+
+
+//create a for loop
+
+for (let i = 0; i < list.length; i ++){
+
+
+if (i >= startIndex && i < endIndex){
+   list[i].style.display = 'block';
+}else{
+
+   list[i].style.display = 'none';
+}
+
+
+
+}
+
+
+
+}
+
+console.log(showPage());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
