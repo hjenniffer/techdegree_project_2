7033c76cbf31475d84b96508b82839570a10f512
Jenn <hjenniffer@hotmail.com>
2019-10-26 16:38:03 -0700

    update search bar

--- js/script.js
+++ js/script.js
@@ -63,7 +63,7 @@ if (i >= startIndex && i < endIndex){
 
 const appendPageLinks = (list)=>{
 
-   // Determine how many pages are needed for the list store in a variable
+   // Determine how many pages are needed for the list ,store in a variable
    
    const totalNumberofPages = Math.ceil (list.length/itemsPerPage);
    const page = document.querySelector('.page'); 
@@ -75,7 +75,7 @@ const appendPageLinks = (list)=>{
 
 
 
-//create a loop to show at the bottom of the pages
+//create a loop to show the pagination at the bottom of the pages
 for ( let i = 1; i <= totalNumberofPages; i+=1){
 
 const li = document.createElement('li');
@@ -122,20 +122,110 @@ input.placeholer = 'Search for students';
 const searchButton = document.createElement('button');
 searchButton.textContent = 'Search';
 
+//use append Child to diplay on page
+
 pageDiv.appendChild(searchDiv);
 searchDiv.appendChild(input);
 searchDiv.appendChild(searchButton);
 
+// create searchStudent function to store values for the student name which are in the h3 element
+
+function searchStudents  (searchInput){
+ const studentNames = document.querySelectorAll('h3');
+
+ //store the value entered that is entered into the search field
+
+ const searchValue = document.querySelectorAll('input').values;
+ // create const set to an empty array that will be assigned a value in the for loop 
+
+ const searchResults = [];
+
+ //create a for loop to go through student names
+
+ for( i= 0; i < studentNames.length; i ++){
+
+   // create query variable to store user input set to all lowercase; This will help validate the data 
+
+   const query = searchInput.toLowerCase();
+
+   //conditinal statement : if results found , add them to result array & display student names
+   if (studentNames[i].textContent.toLowerCase().includes(query) 
+    && searchInput.length !==0 ) {
+       studentNames[i].style.display = 'block';
+       searchResults.push(studentListItem[i]);
+    }else if(searchInput.length ===0){
+
+      showPage(studentListItem,1);
+
+    }else {
+
+      studentListItem[i].style.display ='none';
+
+    }
+
+
+
+    }
 
 
 
 
+  //add no results message if no names are found
+  const noResults = document.querySelectorAll('p');
+  console.log(noResults);
+  if (noResults !== 'null') {
+    for (let i = 0; i < noResults.length; i++) {
+      noResults[i].remove();
+    }
+  }
+  // check if pagination already exists, if so, remove it (not removing this class adds duplicate paginations)
+  const isPaginationActive = document.querySelector('.pagination');
+  if (isPaginationActive) {
+    isPaginationActive.remove();
+  }
+  // if no names were found to match the input value, show the no results message
+  if (searchInput.length === 0) {
+    appendPageLinks(studentListItem);
+  } else if (searchResults.length === 0) {
+    const page = document.querySelector('.page');
+    const noNames = document.createElement('p');
+    noNames.textContent = 'No results were found.';
+    noNames.style.color = 'red';
+    page.appendChild(noNames);
+  } else {
+    appendPageLinks(searchResults);
+    showPage(searchResults, 1);
+  }
+}
 
+searchButton.addEventListener('click', () => {
+  searchStudents(input.value);
+});
+input.addEventListener('keyup', () => {
+  searchStudents(input.value);
+});
 
-
-
+showPage(studentListItem, 1);
 appendPageLinks(studentListItem);
-showPage(studentListItem,1);
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
